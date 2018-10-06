f72a6181667a89231052e08541c21efb6386b13a
AnngelNts <Anngel.Nts@gmail.com>
2018-10-05 21:51:23 -0500

    Agregando AutorMapeo y Entidad Autor

---
+++ Repositorio.Infraestructura.Datos/Mapeos/AutorMapeo.cs
@@ -0,0 +1,18 @@
+﻿using Repositorio.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.Infraestructura.Datos.Mapeos
+{
+    public class AutorMapeo : EntityTypeConfiguration<Autor>
+    {
+        public AutorMapeo()
+        {
+            ToTable("TBL_AUTOR");
+        }
+    }
+}
