b2a548d08f170dde794e0d21e6202fc9b62c4ca8
unknown <percy-taquila23@hotmail.com>
2018-10-08 17:13:10 -0500

    subiendo mi parte

---
+++ Repositorio.Dominio/Repositorio.PruebasUnitarias/FicheroPruebasUnitariasDatos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repositorio.Dominio.Entidades;
+using Repositorio.Infraestructura.Datos;
+
+namespace Repositorio.PruebasUnitarias
+{
+    [TestClass]
+    public class FicheroPruebasUnitariasDatos
+    {
+        [TestMethod]
+        public void CrearAutorSatisfactoriamente()
+        {
+            var loFichero = Fichero.Registrar("2018_001", "limache-sandoval-elmer.pdf","ninguno", "http://repositorio.upt.edu.pe/bitstream/UPT/224/1/Apaza-Mamani-Karla.pdf", "3,42mb", "PDF",1);
+            var loRepositorio = new Repositorio();
+            loRepositorio.Adicionar<Fichero>(loFichero);
+            loRepositorio.GuardarCambios();
+            Assert.IsNotNull(loFichero);
+            Assert.IsTrue(loFichero.IdFichero == "2018_001");
+        }
+        [TestMethod]
+        public void CambiarNombreFicheroSatisfactoriamente()
+        {
+            var loRepositorio = new Repositorio();
+            var loFichero = loRepositorio.ObtenerPorId<Fichero>(1);
+            loFichero.CambiarNombre("Ccama-Perez-Oracio.pdf");
+            loRepositorio.GuardarCambios();
+            Assert.IsTrue(loFichero.Nombre == "Ccama-Perez-Oracio.pdf");
+        }
+
+        [TestMethod]
+        public void CambiarNombreFichero02Satisfactoriamente()
+        {
+            var loRepositorio = new Repositorio();
+            var loFichero = loRepositorio.Listar<Fichero>().FirstOrDefault(p => p.Nombre == "limache-sandoval-elmer.pdf");
+            loFichero.CambiarNombre("Zapana-Zapana-Ruth.pdf");
+            loRepositorio.GuardarCambios();
+            Assert.IsNotNull(loFichero);
+        }
+    }
+}
